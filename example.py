c7011f85a14cd924f8da43b6b8bbae0a88ef5d81
Daniel M. Lofaro <dan@danlofaro.com>
2013-10-24 11:36:37 -0400

    seems to install correctly

---
+++ example.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# /* -*-  indent-tabs-mode:t; tab-width: 8; c-basic-offset: 8  -*- */
+# /*
+# Copyright (c) 2013, Daniel M. Lofaro
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the author nor the names of its contributors may
+#       be used to endorse or promote products derived from this software
+#       without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# */
+
+import zmq
+import time
+context = zmq.Context()
+ 
+subscriber = context.socket (zmq.SUB)
+subscriber.connect ("tcp://192.168.55.112:5556")
+subscriber.connect ("tcp://192.168.55.201:7721")
+subscriber.setsockopt (zmq.SUBSCRIBE, "NASDAQ")
+ 
+publisher = context.socket (zmq.PUB)
+publisher.bind ("ipc://nasdaq-feed")
+ 
+while True:
+    message = subscriber.recv()
+    publisher.send (message)
--- install.sh
+++ install.sh
@@ -8,6 +8,12 @@ make
 sudo make install
 cd $HUBO_VIEW_ROOT
 
+# Python Dep
+sudo apt-get install python-pip
+sudo pip install ipython
+sudo pip install --upgrade --user ipython
+sudo easy_install ipython
+
 # Python Bindings
 sudo easy_install "pyzmq==2.0.10.1"
 cd dep
@@ -15,6 +21,10 @@ tar -xvf pyzmq-13.0.2.tar.gz
 cd pyzmq-13.0.2
 sudo pip install cython
 sudo python setup.py configure --zmq=/usr/local
+sudo python setup.py configure --zmq=../zeromq-3.2.4
+sudo python setup.py install 
+pip install pyzmq --install-option="--zmq=bundled"
+
 
 # Test python install
 sudo python setup.py build_ext --inplace
