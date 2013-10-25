47b5facc52bbcd4ca6f62fd951dec987c07e90b1
Daniel M. Lofaro <dan@danlofaro.com>
2013-10-25 19:01:28 -0400

    weather server example working

--- example.py
+++ example.py
@@ -32,8 +32,8 @@ import time
 context = zmq.Context()
  
 subscriber = context.socket (zmq.SUB)
-subscriber.connect ("tcp://192.168.55.112:5556")
-subscriber.connect ("tcp://192.168.55.201:7721")
+subscriber.connect ("tcp://192.168.0.107:5556")
+#subscriber.connect ("tcp://192.168.55.201:7721")
 subscriber.setsockopt (zmq.SUBSCRIBE, "NASDAQ")
  
 publisher = context.socket (zmq.PUB)
